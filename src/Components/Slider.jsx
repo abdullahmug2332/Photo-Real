765e8899eba6ab9dba1710e9b83be26e849a2ef4
abdullahmug2332 <abdullahmug2332@gmail.com>
2025-04-29 22:06:26 +0500

    ADD slider in top

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -6,8 +6,11 @@ export default function Slider() {
     <>
     <div className='fixed top-0 z-50'>
         <div className="slider-container">
-        <Marquee speed={80} gradient={false} pauseOnHover={false} className='slider-text hover:cursor-pointer'>
-            For US passports: Infants under 3 years old can apply Monday to Friday (8 AM to 10 AM only) — excluding Canadian passport photos. For ages 3 years to adults: Applications are accepted Monday to Friday, 8 AM to 3 PM.
+        <Marquee speed={100} gradient={false} pauseOnHover={false} className='slider-text hover:cursor-pointer'>
+            <p>
+
+            For US passports: Infants under 3 years old can apply Monday to Friday (8 AM to 10 AM only) — excluding Canadian passport photos. 
+            </p>
         </Marquee>
 
       </div>
